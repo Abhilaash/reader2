507d242bd2b87054ffee5460308906d8d3d5ff3c
Abhilaash <Abhilaash.Velamati@gmail.com>
2014-08-08 15:38:48 -0400

    Matched the old file

--- Reader2/app/src/main/java/com/velamati/abhilaash/reader2/MainActivity.java
+++ Reader2/app/src/main/java/com/velamati/abhilaash/reader2/MainActivity.java
@@ -1,20 +1,52 @@
 package com.velamati.abhilaash.reader2;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+//import android.content.res.AssetManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-//this is a test
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+    public static final String WIFI = "Wi-Fi";
+    public static final String ANY = "Any";
+    public static String sPref = null;
+    private static boolean wifiConnected = false;
+    private static boolean mobileConnected = false;
+    public static boolean refreshDisplay = true;
+    private JSONArray json = null;
+    private ListView listview;
+    private TextView textview;
+    private NetworkReceiver receiver = new NetworkReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview = (ListView) findViewById(R.id.listview);
+        textview = (TextView) findViewById(R.id.rowTextView);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,9 +61,210 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    protected void onStart() {
+        updateConnectedFlags();
+        try {
+            loadPage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        // Unregisters BroadcastReceiver when app is destroyed.
+        if (receiver != null) {
+            this.unregisterReceiver(receiver);
+        }
+    }
+
+
+    public void loadPage() throws IOException, JSONException {
+        if (((sPref.equals(ANY)) && (wifiConnected || mobileConnected))
+                || ((sPref.equals(WIFI)) && (wifiConnected))) {
+            // AsyncTask subclass
+            new download().execute("http://www.antarice.com/concepts/vnotam/document.json");
+        } else {
+            showErrorPage();
+        }
+    }
+
+    public void showErrorPage() {
+        textview.setText("ERROR");
+    }
+
+    private void updateConnectedFlags() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Retrieves a string value for the preferences. The second parameter
+        // is the default value to use if a preference value is not found.
+        sPref = sharedPrefs.getString("listPref", "Wi-Fi");
+        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
+        if (activeInfo != null && activeInfo.isConnected()) {
+            wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+            mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+        } else {
+            wifiConnected = false;
+            mobileConnected = false;
+        }
+    }
+
+    private class download extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return loadFromNetwork(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                json = new JSONArray(result);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            displayJSON();
+        }
+
+    }
+
+
+    private String loadFromNetwork(String urlString) throws IOException {
+        InputStream stream = null;
+        String str = "";
+        try {
+            stream = downloadUrl(urlString);
+            str = readIt(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return str;
+    }
+
+    /**
+     * Given a string representation of a URL, sets up a connection and gets
+     * an input stream.
+     *
+     * @param urlString A string representation of a URL.
+     * @return An InputStream retrieved from a successful HttpURLConnection.
+     * @throws java.io.IOException
+     */
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Start the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+
+
+    protected String readIt(InputStream stream) throws IOException {
+        String a = "";
+        InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+        if (reader.ready()) {
+            BufferedReader br = new BufferedReader(reader);
+            String line = br.readLine();
+            while (line != null) {
+                a += line;
+                line = br.readLine();
+            }
+            br.close();
+        }
+        return a;
+    }
+
+
+    private void displayJSON(){
+        ArrayList<String> al = new ArrayList<String>();
+        for (int x = 0; x < json.length(); x++) {
+            try {
+                al.add(json.getJSONObject(x).getString("notamnumber") + "\n" + json.getJSONObject(x).getString("notamtext"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, al);
+        listview.setAdapter(listAdapter);
+    }
+
+    public class NetworkReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager conn = (ConnectivityManager)
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = conn.getActiveNetworkInfo();
+
+            // Checks the user prefs and the network connection. Based on the result, decides whether
+            // to refresh the display or keep the current display.
+            // If the userpref is Wi-Fi only, checks to see if the device has a Wi-Fi connection.
+            if (WIFI.equals(sPref) && networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                // If device has its Wi-Fi connection, sets refreshDisplay
+                // to true. This causes the display to be refreshed when the user
+                // returns to the app.
+                refreshDisplay = true;
+                //         Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT).show();
+
+                // If the setting is ANY network and there is a network connection
+                // (which by process of elimination would be mobile), sets refreshDisplay to true.
+            } else if (ANY.equals(sPref) && networkInfo != null) {
+                refreshDisplay = true;
+
+                // Otherwise, the app can't download content--either because there is no network
+                // connection (mobile or Wi-Fi), or because the pref setting is WIFI, and there
+                // is no Wi-Fi connection.
+                // Sets refreshDisplay to false.
+            } else {
+                refreshDisplay = false;
+                //          Toast.makeText(context, R.string.lost_connection, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
+//    public HttpURLConnection open(URL url) {
+//        HttpURLConnection conn = null;
+//        try {
+//            conn = (HttpURLConnection) url.openConnection();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        updateConnectedFlags();
+//        return conn;
+//    }
+
+
+//    public JSONArray readIt() throws IOException, JSONException {
+//        AssetManager am = this.getAssets();
+//        System.out.print(am.getLocales());
+//        InputStream stream = am.open("document.json");
+//        InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+//        String a = "";
+//        if (reader.ready()) {
+//            BufferedReader br = new BufferedReader(reader);
+//            String line = br.readLine();
+//            while (line != null) {
+//                a += line;
+//                line = br.readLine();
+//            }
+//            br.close();
+//        }
+//        return new JSONArray(a);
+//    }
+
